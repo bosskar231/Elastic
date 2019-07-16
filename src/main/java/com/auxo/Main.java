604f8510c4877f482ead0c4be5c34fdbf585b11c
baskar <bosskar231@gmail.com>
2019-07-16 18:05:43 +0530

    CRUD Operations Completed

--- src/main/java/com/auxo/Main.java
+++ src/main/java/com/auxo/Main.java
@@ -4,6 +4,7 @@ package com.auxo;
 
 import com.auxo.model.Employee;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
@@ -12,16 +13,22 @@ import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
 import org.apache.http.HttpHost;
 import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Main  {
@@ -42,55 +49,190 @@ public class Main  {
                     RestClient.builder(
                             new HttpHost(HOST, PORT_ONE, SCHEME),
                             new HttpHost(HOST, PORT_TWO, SCHEME)));
+            System.out.println("Connnection done");
         }
 
         return restHighLevelClient;
     }
+    private static Employee updateEmployeeById(String id, Employee employee){
+        UpdateRequest updateRequest = new UpdateRequest(INDEX, TYPE, id).fetchSource(true);
+        try {
+            String personJson = objectMapper.writeValueAsString(employee);
+            updateRequest.doc(personJson, XContentType.JSON);
+            UpdateResponse updateResponse = restHighLevelClient.update(updateRequest);
+            return objectMapper.convertValue(updateResponse.getGetResult().sourceAsMap(), Employee.class);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Unable to update ");
+        return null;
+    }
 
+    private static void deleteEmployeeById(String id) {
+        DeleteRequest deleteRequest = new DeleteRequest(INDEX, TYPE, id);
+        try {
+            DeleteResponse deleteResponse = restHighLevelClient.delete(deleteRequest);
+        } catch (Exception e){
+            System.out.println("error in deleting...");
+           e.printStackTrace();
+        }
+    }
     private static synchronized void closeConnection() throws IOException {
         restHighLevelClient.close();
         restHighLevelClient = null;
     }
-    private static Employee insertPerson(Employee employee){
-        System.out.println("person inserted");
-        employee.setEid(UUID.randomUUID().toString());
+    private static Employee insertEmployee(Employee employee){
+
+        employee.setEid("10928");
 
         Map<String, Object> dataMap = new HashMap<String, Object>();
         dataMap.put("eid", employee.getEid());
         dataMap.put("name", employee.getName());
+        dataMap.put("age",employee.getAge());
+        dataMap.put("place",employee.getPlace());
+        dataMap.put("dob",employee.getDob());
+        dataMap.put("phone",employee.getPhone());
         IndexRequest indexRequest = new IndexRequest(INDEX, TYPE, employee.getEid())
                 .source(dataMap);
         try {
             IndexResponse response = restHighLevelClient.index(indexRequest);
-        } catch(ElasticsearchException e) {
-            e.getDetailedMessage();
-        } catch (java.io.IOException ex){
-            ex.getLocalizedMessage();
+        } catch (Exception e){
+            e.printStackTrace();
         }
+        System.out.println("Employee Inserted ");
         return employee;
     }
 
     private static Employee getEmployeeById(String id){
-        GetRequest getPersonRequest = new GetRequest(INDEX, TYPE, id);
+        GetRequest getPersonRequest = new GetRequest(INDEX, TYPE,id);
         GetResponse getResponse = null;
         try {
             getResponse = restHighLevelClient.get(getPersonRequest);
-        } catch (java.io.IOException e){
-            e.getLocalizedMessage();
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        return getResponse != null ?
-                objectMapper.convertValue(getResponse.getSourceAsMap(), Employee.class) : null;
+        return getResponse != null ? objectMapper.convertValue(getResponse.getSourceAsMap(), Employee.class) : null;
     }
     public static void main(String[] args) throws Exception {
+        try {
+            makeConnection();
+            Employee employee = new Employee();
+            while (true) {
+                System.out.println("Enter the values for operations");
+                System.out.println("1.Insert\n2.display\n3.update\n4.delete\n5.exit");
+                Scanner scanner = new Scanner(System.in);
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
 
-        makeConnection();
 
-        Employee employee = new Employee();
-        employee.setName("Baskar");
-        employee =       insertPerson(employee);
-        Employee  personFromDB = getEmployeeById(employee.getEid());
+                    case 1:
+                        System.out.println("the employee fiels are \n1.id\n2.name\n3.age\n4.place\n5.dob\n6.phone");
+                        System.out.println("fill the fields");
+                        System.out.println("enter name ");
+                        String name = scanner.nextLine();
+                        System.out.println("enter age");
+                        String age = scanner.nextLine();
+                        System.out.println("enter place");
+                        String place = scanner.nextLine();
+                        System.out.println("enter the dob");
+                        String dob = scanner.nextLine();
+                        System.out.println("enter the phone");
+                        String phone = scanner.nextLine();
+                        System.out.println("object creation in progress.....");
+                        Thread.sleep(2000);
+                        employee.setName(name);
+                        employee.setAge(age);
+                        employee.setPlace(place);
+                        employee.setDob(dob);
+                        employee.setPhone(phone);
+                        System.out.println("object created");
+                        employee = insertEmployee(employee);
+                        System.out.println("inserted employee " + employee.toString());
+
+                        break;
+                    case 2:
+                        System.out.println("enter the id to search");
+                        String id = scanner.nextLine();
+                        employee = getEmployeeById(id);
+                        if (employee == null) {
+                            System.out.println("no matching");
+                        } else {
+                            System.out.println(employee.toString());
+                        }
+
+                        break;
+                    case 3:
+                        System.out.println("enter the id  ");
+                        String ids = scanner.nextLine();
+                        System.out.println("select the field to update");
+                        System.out.println("1.name\n2.age\n3.place\n4.dob\n5.phone");
+
+                        int value = scanner.nextInt();
+                        scanner.nextLine();
+                        switch (value) {
+                            case 1:
+                                System.out.println("enter the name");
+                                employee.setName(scanner.nextLine());
+                                employee = updateEmployeeById(employee.getEid(), employee);
+
+                                break;
+                            case 2:
+                                System.out.println("enter the age");
+                                employee.setAge(scanner.nextLine());
+                                employee = updateEmployeeById(employee.getEid(), employee);
+                                break;
+                            case 3:
+                                System.out.println("enter the place");
+                                employee.setPlace(scanner.nextLine());
+                                employee = updateEmployeeById(employee.getEid(), employee);
+                                break;
+                            case 4:
+                                System.out.println("enter the dob");
+                                employee.setDob(scanner.nextLine());
+                                employee = updateEmployeeById(employee.getEid(), employee);
+                                break;
+                            case 5:
+                                System.out.println("enter the phone");
+                                employee.setPhone(scanner.nextLine());
+                                employee = updateEmployeeById(employee.getEid(), employee);
+                                break;
+
+                        }
 
 
+                        break;
+                    case 4:
+                        System.out.println("enter the id to delete");
+                        String idss = scanner.nextLine();
+                        deleteEmployeeById(idss);
+                        break;
+                    case 5:
+                        System.exit(0);
+                        break;
+                }
+
+//
+//            Employee employee = new Employee();
+//            employee.setName("Baskar");
+//            employee = insertEmployee(employee);
+//            Employee personFromDB = getEmployeeById(employee.getEid());
+//            System.out.println("the result " + personFromDB);
+//            System.out.println("Changing name to Baskar s...");
+//            employee.setName("Baskar s");
+//            updateEmployeeById(employee.getEid(), employee);
+//            System.out.println("Person updated  --> " + employee);
+//
+//            System.out.println("Deleting baskar s...");
+//            deleteEmployeeById(personFromDB.getEid());
+//            System.out.println("Person Deleted");
+
+
+            }
+        }
+        finally{
+            closeConnection();
+        }
     }
 
 
--- src/main/java/com/auxo/model/Employee.java
+++ src/main/java/com/auxo/model/Employee.java
@@ -4,38 +4,37 @@ public class Employee {
 
     private String eid;
     private String name;
-//    private int age;
-//    private String place;
-//    private String dob;
-//    private String phone;
-//    private int deptid;
-//    public Employee()
-//    {
-//
-//    }
+    private String  age;
+    private String place;
+    private String dob;
+    private String phone;
+
+    public Employee()
+    {
+
+    }
 
     @Override
     public String toString() {
         return "EmployeeApplication{" +
                 "eid=" + eid +
                 ", name='" + name + '\'' +
-//                ", age=" + age +
-//                ", place='" + place + '\'' +
-//                ", dob='" + dob + '\'' +
-//                ", phone='" + phone + '\'' +
-//                ", deptid=" + deptid +
+                ", age=" + age +
+                ", place='" + place + '\'' +
+                ", dob='" + dob + '\'' +
+                ", phone='" + phone + '\'' +
                 '}';
     }
-//
-//    public Employee(String eid, String name) {
-//        this.eid = eid;
-//        this.name = name;
-////        this.age = age;
-////        this.place = place;
-////        this.dob = dob;
-////        this.phone = phone;
-////        this.deptid = deptid;
-//    }
+
+    public Employee(String eid, String name,String age,String place,String dob,String phone) {
+        this.eid = eid;
+        this.name = name;
+        this.age = age;
+        this.place = place;
+        this.dob = dob;
+        this.phone = phone;
+
+    }
 
     public String getEid() {
         return eid;
@@ -53,45 +52,38 @@ public class Employee {
         this.name = name;
     }
 
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//
-//    public String getPlace() {
-//        return place;
-//    }
-//
-//    public void setPlace(String place) {
-//        this.place = place;
-//    }
-//
-//    public String getDob() {
-//        return dob;
-//    }
-//
-//    public void setDob(String dob) {
-//        this.dob = dob;
-//    }
-//
-//    public String getPhone() {
-//        return phone;
-//    }
-//
-//    public void setPhone(String phone) {
-//        this.phone = phone;
-//    }
-//
-//    public int getDeptid() {
-//        return deptid;
-//    }
-//
-//    public void setDeptid(int deptid) {
-//        this.deptid = deptid;
-//    }
+    public String  getAge() {
+        return age;
+    }
+
+    public void setAge(String  age) {
+        this.age = age;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 
 
 
